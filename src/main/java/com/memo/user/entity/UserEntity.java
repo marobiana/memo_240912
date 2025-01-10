85d6fce7ed444afa4713f63f7062022c41ff3ba9
DESKTOP-HME3UEB\403megait <test@test.com>
2025-01-10 17:31:33 +0900

    아이디 중복확인 및 회원가입

--- src/main/java/com/memo/user/entity/UserEntity.java
+++ src/main/java/com/memo/user/entity/UserEntity.java
@@ -1,13 +1,34 @@
 package com.memo.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "user")
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "loginId")
     private String loginId;
+
     private String password;
+
     private String name;
+
     private String email;
+
+    @Column(name = "createdAt")
     private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt")
     private LocalDateTime updatedAt;
 }
