224f7c2abc905e863a09040e2245b361bf1f5088
DESKTOP-HME3UEB\403megait <test@test.com>
2025-01-16 17:35:56 +0900

    글 상세 마크업과 내용 가져오기

--- src/main/java/com/memo/post/PostRestController.java
+++ src/main/java/com/memo/post/PostRestController.java
@@ -1,28 +1,47 @@
 package com.memo.post;
 
+import com.memo.post.bo.PostBO;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RequestMapping("/post")
 @RestController
 public class PostRestController {
+
+    private final PostBO postBO;
 
     @PostMapping("/create")
     public Map<String, Object> create(
             @RequestParam("subject") String subject,
             @RequestParam("content") String content,
+            @RequestParam(value = "file", required = false) MultipartFile file,
             HttpSession session
     ) {
         // 글쓴이 번호 꺼내기
         int userId = (int)session.getAttribute("userId");
+        String userLoginId = (String)session.getAttribute("userLoginId");
 
         // db insert
+        int rowCount = postBO.addPost(userId, userLoginId, subject, content, file);
         
         // 응답값
+        Map<String, Object> result = new HashMap<>();
+        if (rowCount > 0) {
+            result.put("code", 200);
+            result.put("result", "성공");
+        } else {
+            result.put("code", 500);
+            result.put("error_message", "글쓰기에 실패했습니다.");
+        }
+        return result;
     }
 }
--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -1,10 +1,12 @@
 package com.memo.post.bo;
 
+import com.memo.common.FileManagerService;
 import com.memo.post.domain.Post;
 import com.memo.post.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,6 +15,7 @@ import java.util.List;
 public class PostBO {
 
     private final PostMapper postMapper;
+    private final FileManagerService fileManager;
 //    private final UserBO userBO;
 //
 //    public PostBO(PostMapper postMapper, UserBO userBO) {
@@ -24,5 +27,26 @@ public class PostBO {
     // o: List<Post>
     public List<Post> getPostListByUserId(int userId) {
         return postMapper.selectPostListByUserId(userId);
+    }
+
+    // i: postId, userId
+    // o: Post or null
+    public Post getPostByPostIdUserId(int postId, int userId) {
+        return postMapper.selectPostByPostIdUserId(postId, userId);
+    }
+
+    // i: userId, userLoginId, subject, content, file
+    // o: int(성공 행 개수)
+    public int addPost(int userId, String userLoginId,
+                       String subject, String content, MultipartFile file) {
+
+        String imagePath = null;
+
+        // 파일 존재시 업로드
+        if (file != null) {
+            imagePath = fileManager.uploadFile(file, userLoginId);
+        }
+
+        return postMapper.insertPost(userId, subject, content, imagePath);
     }
 }
